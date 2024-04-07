bcadfd225604c82068bb99a8a4abfca347feb476
swathiraj <swathiraj687@gmail.com>
2024-04-07 12:57:38 +0000

    solution

---
+++ src/main/java/com/example/wordlyweek/model/Magazine.java
@@ -0,0 +1,74 @@
+/*
+ * You can use the following import statements
+ *
+ * import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+ * 
+ * import javax.persistence.*;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.wordlyweek.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "magazine")
+public class Magazine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int magazineId;
+    @Column(name = "title")
+    private String magazineName;
+    @Column(name = "publicationdate")
+    private String publicationDate;
+    @ManyToMany(mappedBy = "magazines")
+    @JsonIgnoreProperties("magazines")
+    private List<Writer> writers = new ArrayList<>();
+
+    public Magazine(int magazineId, String magazineName, String publicationDate, List<Writer> writers) {
+        this.magazineId = magazineId;
+        this.magazineName = magazineName;
+        this.publicationDate = publicationDate;
+        this.writers = writers;
+    }
+    public Magazine(){}
+
+    public int getMagazineId() {
+        return magazineId;
+    }
+
+    public void setMagazineId(int magazineId) {
+        this.magazineId = magazineId;
+    }
+
+    public String getMagazineName() {
+        return magazineName;
+    }
+
+    public void setMagazineName(String magazineName) {
+        this.magazineName = magazineName;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public List<Writer> getWriters() {
+        return writers;
+    }
+
+    public void setWriters(List<Writer> writers) {
+        this.writers = writers;
+    }
+}
---
+++ src/main/java/com/example/wordlyweek/model/Writer.java
@@ -0,0 +1,79 @@
+/*
+ * You can use the following import statements
+ *
+ * import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+ * 
+ * import javax.persistence.*;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.wordlyweek.model;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "writer")
+public class Writer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int writerId;
+    @Column(name = "name")
+    private String writerName;
+    @Column(name = "bio")
+    private String bio;
+
+    @ManyToMany
+    @JoinTable(name = "writer_magazine", joinColumns = @JoinColumn(name = "writerid"), inverseJoinColumns = @JoinColumn(name = "magazineid"))
+    @JsonIgnoreProperties("writers")
+    private List<Magazine> magazines = new ArrayList<>();
+
+    public Writer(int writerId, String writerName, String bio, List<Magazine> magazines) {
+        this.writerId = writerId;
+        this.writerName = writerName;
+        this.bio = bio;
+        this.magazines = magazines;
+    }
+
+    public Writer() {
+    }
+
+    public int getWriterId() {
+        return writerId;
+    }
+
+    public void setWriterId(int writerId) {
+        this.writerId = writerId;
+    }
+
+    public String getWriterName() {
+        return writerName;
+    }
+
+    public void setWriterName(String writerName) {
+        this.writerName = writerName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public List<Magazine> getMagazines() {
+        return magazines;
+    }
+
+    public void setMagazines(List<Magazine> magazines) {
+        this.magazines = magazines;
+    }
+}
